c36673714c6e9111d9a5c53c0fc1002e3be1638b
primer_programa <elmosabala.dunamis@gmail.com>
2023-07-24 14:33:50 -0500

    subida inicial

---
+++ src/test/java/com/prueba/challenge/Challenge5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
